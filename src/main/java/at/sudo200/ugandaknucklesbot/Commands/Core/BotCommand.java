fd67f2c4dbb8a6d64ba21999c21c867f262f76c1
sudo200 <65343982+sudo200@users.noreply.github.com>
2021-06-13 05:52:20 +0200

    more comments

--- src/main/java/at/sudo200/ugandaknucklesbot/Commands/Core/BotCommand.java
+++ src/main/java/at/sudo200/ugandaknucklesbot/Commands/Core/BotCommand.java
@@ -1,5 +1,7 @@
 package at.sudo200.ugandaknucklesbot.Commands.Core;
 
+// abstract class which gets inherited
+// by every command class
 public abstract class BotCommand {
     protected abstract String setName();
     protected abstract void execute(CommandParameter param);

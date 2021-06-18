bb29c86698148b70ae839b89425672b63cf5f5d6
sudo200 <65343982+sudo200@users.noreply.github.com>
2021-06-18 18:58:53 +0200

    Added Annotation

--- src/main/java/at/sudo200/ugandaknucklesbot/Commands/ChatCommandInator.java
+++ src/main/java/at/sudo200/ugandaknucklesbot/Commands/ChatCommandInator.java
@@ -4,12 +4,13 @@ import at.sudo200.ugandaknucklesbot.Commands.Core.BotCommand;
 import at.sudo200.ugandaknucklesbot.Commands.Core.CommandParameter;
 import at.sudo200.ugandaknucklesbot.Util.UtilsChat;
 import net.dv8tion.jda.api.EmbedBuilder;
+import org.jetbrains.annotations.NotNull;
 
 public class ChatCommandInator extends BotCommand {
     private final UtilsChat utilsChat = new UtilsChat();
 
     @Override
-    protected String setName() {
+    protected @NotNull String setName() {
         return "inator";
     }
 
--- src/main/java/at/sudo200/ugandaknucklesbot/Commands/Core/BotCommand.java
+++ src/main/java/at/sudo200/ugandaknucklesbot/Commands/Core/BotCommand.java
@@ -1,8 +1,10 @@
 package at.sudo200.ugandaknucklesbot.Commands.Core;
 
+import org.jetbrains.annotations.NotNull;
+
 // abstract class which gets inherited
 // by every command class
 public abstract class BotCommand {
-    protected abstract String setName();
+    protected abstract @NotNull String setName();
     protected abstract void execute(CommandParameter param);
 }

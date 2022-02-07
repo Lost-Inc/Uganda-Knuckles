591eefcc15ad884598edf6ab15170a6fc5081621
sudo200 <65343982+sudo200@users.noreply.github.com>
2022-02-07 17:58:05 +0100

    Add ChatCommandInsult

---
+++ src/main/java/at/lost_inc/ugandaknucklesbot/Commands/Classes/Chat/ChatCommandInsult.java
@@ -0,0 +1,44 @@
+package at.lost_inc.ugandaknucklesbot.Commands.Classes.Chat;
+
+import at.lost_inc.ugandaknucklesbot.Commands.API.BotCommand;
+import at.lost_inc.ugandaknucklesbot.Commands.API.Command;
+import at.lost_inc.ugandaknucklesbot.Commands.API.CommandParameter;
+import at.lost_inc.ugandaknucklesbot.Commands.Classes.JSONTypeClasses.EvilInsultAPIResponse;
+import at.lost_inc.ugandaknucklesbot.Service.ServiceManager;
+import at.lost_inc.ugandaknucklesbot.Util.UtilsChat;
+import com.github.kevinsawicki.http.HttpRequest;
+import com.google.gson.Gson;
+import net.dv8tion.jda.api.EmbedBuilder;
+import org.jetbrains.annotations.NotNull;
+
+@Command(
+        name = "insult",
+        help = "Insults the channel with a random, but evil insult\n" +
+                "...",
+        categories = {
+                BotCommand.ICategories.FUN,
+                BotCommand.ICategories.CHAT
+        }
+)
+public class ChatCommandInsult extends BotCommand {
+    private final UtilsChat utilsChat = ServiceManager.provideUnchecked(UtilsChat.class);
+    private final Gson gson = ServiceManager.provideUnchecked(Gson.class);
+
+    @Override
+    public void execute(@NotNull CommandParameter param) {
+        EmbedBuilder builder = utilsChat.getDefaultEmbed();
+        String response = HttpRequest.get("https://evilinsult.com/generate_insult.php?lang=en&type=json").body();
+        try {
+            EvilInsultAPIResponse evilInsultAPIResponse = gson.fromJson(response, EvilInsultAPIResponse.class);
+            builder.setAuthor(evilInsultAPIResponse.createdby);
+            builder.setDescription(evilInsultAPIResponse.insult);
+
+            utilsChat.send(param.message.getChannel(), builder.build());
+        } catch (Exception e) {
+            utilsChat.sendInfo(
+                    param.message.getChannel(),
+                    "**Error** could not load insult\n\nF\\*\\*\\* you!"
+            );
+        }
+    }
+}
---
+++ src/main/java/at/lost_inc/ugandaknucklesbot/Commands/Classes/JSONTypeClasses/EvilInsultAPIResponse.java
@@ -0,0 +1,12 @@
+package at.lost_inc.ugandaknucklesbot.Commands.Classes.JSONTypeClasses;
+
+public class EvilInsultAPIResponse {
+    public String number;
+    public String language;
+    public String insult;
+    public String created;
+    public String shown;
+    public String createdby;
+    public String active;
+    public String comment;
+}

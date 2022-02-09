ac12d9af2d66c45c5bfdce1732c184a0e05182ad
sudo200 <65343982+sudo200@users.noreply.github.com>
2022-02-09 15:29:40 +0100

    Add time of last message to ChatCommandServerStats

--- src/main/java/at/lost_inc/ugandaknucklesbot/Commands/Classes/Chat/ChatCommandServerStats.java
+++ src/main/java/at/lost_inc/ugandaknucklesbot/Commands/Classes/Chat/ChatCommandServerStats.java
@@ -5,12 +5,13 @@ import at.lost_inc.ugandaknucklesbot.Commands.API.Command;
 import at.lost_inc.ugandaknucklesbot.Commands.API.CommandParameter;
 import at.lost_inc.ugandaknucklesbot.Service.ServiceManager;
 import at.lost_inc.ugandaknucklesbot.Util.UtilsChat;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 
@@ -32,15 +33,16 @@ public final class ChatCommandServerStats extends BotCommand {
 
     @Override
     public void execute(@NotNull CommandParameter param) {
+        final Message message = utilsChat.sendInfo(param.message.getChannel(), "Getting info...");
+
         final Guild guild = param.message.getGuild();
         final List<Member> members = guild.getMembers();
+        final Optional<Message> newestMsg = getLastMessage(guild.getChannels(), param.message.getIdLong());
         final int all = members.size();
         final int boosters = guild.getBoosters().size();
         final int bots = getBots(members).size();
 
-
-        utilsChat.send(
-                param.message.getChannel(),
+        message.editMessage(
                 utilsChat.getDefaultEmbed()
                         .setTitle(String.format("Statistics for \"%s\"", guild.getName()))
                         .setThumbnail(guild.getIconUrl())
@@ -54,16 +56,39 @@ public final class ChatCommandServerStats extends BotCommand {
                         ), false)
                         .addField("Other stuff", String.format(
                                 "This server is %d years and %d months old\n" +
-                                        "It has %d boosts, which brings it to level %d",
+                                        "It has %d boosts, which brings it to level %d\n" +
+                                        (newestMsg.isPresent() ? String.format(
+                                                "The last message was created on\n**%s**",
+                                                newestMsg.get().getTimeCreated().format(DateTimeFormatter.RFC_1123_DATE_TIME)
+                                        ) : ""),
                                 OffsetDateTime.now().getYear() - guild.getTimeCreated().getYear(),
                                 OffsetDateTime.now().getMonthValue() - guild.getTimeCreated().getMonthValue(),
                                 guild.getBoostCount(), guild.getBoostTier().getKey()
                         ), false).build()
-                );
+        ).complete();
     }
 
 
     private @NotNull List<Member> getBots(@NotNull List<Member> members) {
         return members.stream().filter(m -> m.getUser().isBot()).collect(Collectors.toList());
+    }
+
+    private @NotNull Optional<Message> getLastMessage(@NotNull List<GuildChannel> channels, long markerMsg) {
+        Message newestMsg = null;
+        for(GuildChannel channel : channels) {
+            if(!ChannelType.TEXT.equals(channel.getType()))
+                continue;
+
+            final TextChannel textChannel = channel.getGuild().getTextChannelById(channel.getIdLong());
+            if(textChannel == null)
+                continue;
+            final List<Message> msgs = textChannel.getHistoryBefore(markerMsg, 1).complete().getRetrievedHistory();
+            if(msgs.size() == 0)
+                continue;
+            if(newestMsg == null || msgs.get(0).getTimeCreated().isAfter(newestMsg.getTimeCreated()))
+                newestMsg = msgs.get(0);
+        }
+
+        return Optional.ofNullable(newestMsg);
     }
 }

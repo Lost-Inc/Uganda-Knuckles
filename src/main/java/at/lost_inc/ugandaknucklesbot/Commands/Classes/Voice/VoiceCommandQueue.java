75a51aae4fed61eb7dd4338b118c0a509bb47e92
sudo200 <65343982+sudo200@users.noreply.github.com>
2022-02-19 07:40:11 +0100

    Added splitting of queue, if length exceeds 2048 characters

--- src/main/java/at/lost_inc/ugandaknucklesbot/Commands/Classes/Voice/VoiceCommandQueue.java
+++ src/main/java/at/lost_inc/ugandaknucklesbot/Commands/Classes/Voice/VoiceCommandQueue.java
@@ -9,6 +9,7 @@ import at.lost_inc.ugandaknucklesbot.Service.ServiceManager;
 import at.lost_inc.ugandaknucklesbot.Util.UtilsChat;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import org.jetbrains.annotations.NotNull;
 
 
@@ -37,8 +38,8 @@ public final class VoiceCommandQueue extends BotCommand {
     public void execute(@NotNull CommandParameter param) {
         final TrackScheduler scheduler = playerService.getScheduler(param.message.getGuild()).get();
         final TrackScheduler.PlayQueue queue = scheduler.getQueue();
-        final EmbedBuilder builder = utilsChat.getDefaultEmbed().setTitle("Queue");
-        final StringBuilder stringBuilder = new StringBuilder();
+        EmbedBuilder builder = getBuilder();
+        StringBuilder stringBuilder = builder.getDescriptionBuilder();
 
         if(scheduler.getLooping())
             stringBuilder.append("**Looping enabled!**\n\n");
@@ -49,6 +50,15 @@ public final class VoiceCommandQueue extends BotCommand {
                 final AudioTrackInfo info = queue.audioTracks.get(i).getInfo();
                 if(i+1 == queue.getTrackNum())
                     stringBuilder.append("\t\u2554Current Track\n");
+                if((i+1 + ". " + info.author + " - \"" + info.title + '\"').length() + stringBuilder.length() > MessageEmbed.TEXT_MAX_LENGTH) {
+                    stringBuilder.append("```");
+                    utilsChat.send(param.message.getChannel(), builder.build());
+                    builder = getBuilder();
+                    stringBuilder = builder.getDescriptionBuilder();
+                    if(scheduler.getLooping())
+                        stringBuilder.append("**Looping enabled!**\n\n");
+                    stringBuilder.append("```haskell\n");
+                }
                 stringBuilder.append(i+1).append(". ").append(info.author).append(" - \"").append(info.title).append('\"');
                 if(i+1 == queue.getTrackNum())
                     stringBuilder.append("\n\t\u255ACurrent Track");
@@ -57,12 +67,11 @@ public final class VoiceCommandQueue extends BotCommand {
         else
             stringBuilder.append("Nothing to see here!\n");
 
-        builder.setDescription(stringBuilder.append("```"));
+        stringBuilder.append("```");
+        utilsChat.send(param.message.getChannel(), builder.build());
+    }
 
-        try {
-            utilsChat.send(param.message.getChannel(), builder.build());
-        } catch (Throwable e) {
-            utilsChat.sendInfo(param.message.getChannel(), "Sorry, your queue is to big for Discord!");
-        }
+    private @NotNull EmbedBuilder getBuilder() {
+        return utilsChat.getDefaultEmbed().setTitle("Queue");
     }
 }

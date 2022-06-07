8309774d135da2406cbd0a64310dbbcae3547ff8
Lauze <laurenz.zeinzinger@htlstp.at>
2022-06-07 09:25:15 +0200

    Bump Java to 11 Add source managers for Apple Music and Spotify

--- src/main/java/at/lost_inc/ugandaknucklesbot/Service/Audio/SimpleAudioPlayerManagerService.java
+++ src/main/java/at/lost_inc/ugandaknucklesbot/Service/Audio/SimpleAudioPlayerManagerService.java
@@ -1,6 +1,9 @@
 package at.lost_inc.ugandaknucklesbot.Service.Audio;
 
 import at.lost_inc.ugandaknucklesbot.Util.Author;
+import com.github.topislavalinkplugins.topissourcemanagers.applemusic.AppleMusicSourceManager;
+import com.github.topislavalinkplugins.topissourcemanagers.spotify.SpotifyConfig;
+import com.github.topislavalinkplugins.topissourcemanagers.spotify.SpotifySourceManager;
 import com.sedmelluq.discord.lavaplayer.format.StandardAudioDataFormats;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
@@ -15,6 +18,13 @@ public final class SimpleAudioPlayerManagerService implements AudioPlayerManager
         AudioSourceManagers.registerRemoteSources(manager);
         manager.enableGcMonitoring();
         manager.getConfiguration().setOutputFormat(StandardAudioDataFormats.DISCORD_OPUS);
+        manager.registerSourceManager(new AppleMusicSourceManager(null, "AT", manager));
+
+        final SpotifyConfig spotifyConfig = new SpotifyConfig();
+        spotifyConfig.setClientId(System.getenv("SPOTIFYCLIENTID"));
+        spotifyConfig.setClientSecret(System.getenv("SPOTIFYCLIENTSECRET"));
+        spotifyConfig.setCountryCode("AT");
+        manager.registerSourceManager(new SpotifySourceManager(null, spotifyConfig, manager));
     }
 
     @Override

d6cdecb9207894e5beb2187ba61825d8c2dbcb43
sudo200 <65343982+sudo200@users.noreply.github.com>
2022-07-18 11:29:47 +0200

    Catch exception in 'fromObject' method

--- src/main/java/at/lost_inc/ugandaknucklesbot/Util/JsonObjectFactory.java
+++ src/main/java/at/lost_inc/ugandaknucklesbot/Util/JsonObjectFactory.java
@@ -4,11 +4,14 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
 import java.util.Map;
 
 public final class JsonObjectFactory {
+    private static final Logger logger = LoggerFactory.getLogger(JsonObjectFactory.class);
     private static final Gson gson = new Gson();
 
     private JsonObjectFactory() {
@@ -20,21 +23,24 @@ public final class JsonObjectFactory {
         return object;
     }
 
-    public static @NotNull JsonObject fromObject(@NotNull Object obj) throws IllegalAccessException {
+    public static @NotNull JsonObject fromObject(@NotNull Object obj) {
         final JsonObject jsonObject = new JsonObject();
 
-        for(final Field field : obj.getClass().getDeclaredFields()) {
-            field.setAccessible(true);
-            final Object fieldVal = field.get(obj);
-            if(fieldVal instanceof Number)
-                jsonObject.addProperty(field.getName(), (Number) fieldVal);
-            else if(fieldVal instanceof String)
-                jsonObject.addProperty(field.getName(), (String) fieldVal);
-            else if(fieldVal instanceof Character)
-                jsonObject.addProperty(field.getName(), (Character) fieldVal);
-            else if(fieldVal instanceof Boolean)
-                jsonObject.addProperty(field.getName(), (Boolean) fieldVal);
-        }
+        for(final Field field : obj.getClass().getDeclaredFields())
+            try {
+                field.setAccessible(true);
+                final Object fieldVal = field.get(obj);
+                if(fieldVal instanceof Number)
+                    jsonObject.addProperty(field.getName(), (Number) fieldVal);
+                else if(fieldVal instanceof String)
+                    jsonObject.addProperty(field.getName(), (String) fieldVal);
+                else if(fieldVal instanceof Character)
+                    jsonObject.addProperty(field.getName(), (Character) fieldVal);
+                else if(fieldVal instanceof Boolean)
+                    jsonObject.addProperty(field.getName(), (Boolean) fieldVal);
+            } catch (Exception e) {
+                logger.error("Error while building JsonObject!", e);
+            }
 
         return jsonObject;
     }
